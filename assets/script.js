125f62214c75cdfb7ff9e9bb496e45eb5b13e6e5
camilleNicole <camilleht@gmail.com>
2022-01-23 22:35:36 -0500

    fixed color

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ var descriptionEl = document.querySelector(".description");
 var currentDayEl = document.querySelector("#currentDay");
 var saveButtonEl = document.querySelector("saveBtn");
 
-// var timeEl= document.querySelector(".hour");
+
 
 var today = moment(); 
 currentDayEl.textContent = today.format("MMM DD, YYYY - H:MM");
@@ -26,68 +26,33 @@ function loadAppointment() {
 for(var i = 0; i < 10; i++){
 
   var apptKey = `appointment-${i}`;
-  var savedTextArea = localStorage.getItem(apptKey)
+  var savedTextArea = localStorage.getItem(apptKey);
   console.log(savedTextArea)
+  console.log(apptKey);
 
+//  $("#"+ apptKey).val() = savedTextArea;
   document.getElementById(apptKey).value = savedTextArea;
 }
 }
 loadAppointment();
 
+var currentTime= new Date();
+var currentHour= currentTime.getHours();
 
-var time = document.getElementsByClassName(".hour");
-console.log(time);
-let currentHour = parseInt(moment().format('H'));
-
-Array.from(time).forEach(hour => {
-  let
-    hourIdString = hour.id,
-    hourTime;
-  if (hourIdString) {
-    hourTime = parseInt(hourIdString);
-  }
-  if (hourTime) {
-    // Compares row id to current hour and sets color accordingly
-    if (currentHour === hourTime) {
-      descriptionEl.addClass(".present");
-    } else if ((currentHour < hourTime) && (currentHour > hourTime - 6)) {
-      descriptionEl.addClass(".future");
-    } else if ((currentHour > hourTime) && (currentHour < hourTime + 6)) {
-      descriptionEl.addClass(".past");
-    } else {
-      setColor(descriptionEl, "white");
+var auditTime=function() {
+  for(var block of $(".time-block")) {
+    hour = parseInt(block.getAttribute("id"))
+    console.log(hour);
+    if (hour === currentHour) {
+      $(block).addClass("present")
+    } else if (hour < currentHour) {
+      $(block).addClass("past")
+    }
+    else {
+      $(block).addClass("future")
     }
   }
-});
-function setColor(element, color) {
-  element.style.backgroundColor = color;
 }
-//Time block is color-coded to indicate whether it is in the past, present, or future
-// function color() {
-//    get time from hour element
-  
-// var timeColor = $(timeEl).find("h3").text();
-//   console.log(timeEl)
-
-// if(moment().isAfter(9)){
-//   assign
+auditTime();
 
 
-// }
-    // var time = $(".hour").find("h3").text().trim();
-       
-    // // remove any old classes from element
-    // $(timeEl).removeClass("past present future");
-  
-    // // apply new class if task is near/over due date
-    // if (moment().isAfter(time)) {
-    //   $(descriptionEl).addClass("past");
-    // }
-    // // apply new class if appointment is near/over due date
-    // if (moment().isBefore(time)) {
-    //   $(descriptionEl).addClass("future");
-    // } 
-    // else if (Math.abs(moment().diff(time, "hour")) <= 1) {
-    //   $(appointmentEl).addClass("present");
-    // }
-  
